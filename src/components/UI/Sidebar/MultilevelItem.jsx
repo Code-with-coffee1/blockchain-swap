b69b2464a09febaad8ccfa9536b022c6d58bbe44
Husain Shahid Rao <husain@pop-os.localdomain>
2021-09-05 01:52:27 +0530

    navbar improvements

--- src/components/UI/Sidebar/MultilevelItem.jsx
+++ src/components/UI/Sidebar/MultilevelItem.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import ArrowDropDownCircleIcon from "@material-ui/icons/ArrowDropDownCircle";
+import ArrowDropDown from "@material-ui/icons/ArrowDropDown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const SidebarItemContainer = styled.div`
   cursor: pointer;
   text-decoration: none;
@@ -11,11 +13,9 @@ const SidebarItemContainer = styled.div`
   width: 100%;
   margin-left: 0;
   transition: all 0.3s ease-in-out;
- 
 `;
 
 const SidebarTitle = styled.div`
-
   display: flex;
   width: calc(100% - 15px);
 
@@ -25,28 +25,33 @@ const SidebarTitle = styled.div`
     background-color: #4a47a3;
   }
 
-  & span {
+  & span.dropdown-icon {
     display: flex;
     height: 100%;
     align-items: center;
     transform: rotate(${(props) => (props.isOpen ? "0" : "-90deg")});
     transition: all 0.2s ease-in-out;
-
+    margin-left: auto;
+    margin-right: 1rem;
   }
+  & span.icon {
+    display: flex;
+    height: 100%;
+    align-items: center;
+  }
+
   & h3 {
     font-size: 1rem;
     margin-left: 10px;
   }
-
 `;
 
 const DropdownMenu = styled.div`
   width: 100%;
   overflow: hidden;
   max-height: ${(props) => (props.isOpen ? "500px" : "0")};
-  background-color: #2A0944;
+  background-color: #2a0944;
   transition: max-height 0.5s ease-in-out;
- 
 `;
 
 const MultilevelItem = (props) => {
@@ -57,11 +62,14 @@ const MultilevelItem = (props) => {
 
   return (
     <SidebarItemContainer exact={props.exact || false} to={props.to || "/"}>
-      <SidebarTitle isOpen={isOpen}  onClick={expandMenuHandler}>
-        <span className="dropdown-icon">
-          <ArrowDropDownCircleIcon />
+      <SidebarTitle isOpen={isOpen} onClick={expandMenuHandler}>
+        <span className="icon">
+          <FontAwesomeIcon icon={props.icon} />
         </span>
         <h3>{props.title}</h3>
+        <span className="dropdown-icon">
+          <ArrowDropDown />
+        </span>
       </SidebarTitle>
 
       <DropdownMenu isOpen={isOpen}>{props.children}</DropdownMenu>
--- src/components/UI/Sidebar/Sidebar.jsx
+++ src/components/UI/Sidebar/Sidebar.jsx
@@ -1,68 +1,124 @@
 import classes from "./Sidebar.module.css";
 import SidebarItem from "./SidebarItem";
-import HomeIcon from "@material-ui/icons/Home";
-import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
-import LocalShippingIcon from "@material-ui/icons/LocalShipping";
 import SidebarFooter from "./SidebarFooter";
 import MultilevelItem from "./MultilevelItem";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as FontIcons from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = (props) => {
   return (
     <div>
       <div className={props.isOpen && classes.backdrop} />
       <nav className={classes["sidebar-container"] + " " + (props.isOpen && classes["sidebar-open"])}>
-        <SidebarItem icon={HomeIcon} exact={true} to="/">
-          <span>
-            <HomeIcon />
-          </span>
-
-          <h3>Home</h3>
-        </SidebarItem>
-        <SidebarItem to="/trade">
-          <span>
-            <MonetizationOnIcon />
-          </span>
-
-          <h3>Trade</h3>
-        </SidebarItem>
-
-        <SidebarItem to="/farm">
-          <span>
-            <LocalShippingIcon />
-          </span>
-          <h3>Farm</h3>
-        </SidebarItem>
-        <MultilevelItem title={"Dropdown"}>
-          <SidebarItem to="/about">
+        <div className={classes["menu-items"]}>
+          <SidebarItem exact={true} to="/">
             <span>
-              <MonetizationOnIcon />
+              <FontAwesomeIcon icon={FontIcons.faHome} />
             </span>
 
-            <h3>About</h3>
+            <h3>Home</h3>
           </SidebarItem>
-          <SidebarItem to="/lorem">
+          <MultilevelItem icon={FontIcons.faExchangeAlt} title="Trade">
+            <SidebarItem exact={true} to="/exchange">
+              <h3>Exchange</h3>
+            </SidebarItem>
+            <SidebarItem exact={true} to="/liquidity">
+              <h3>Liquidity</h3>
+            </SidebarItem>
+            <SidebarItem exact={true} to="/lp_migration">
+              <h3>LP Migration</h3>
+            </SidebarItem>
+          </MultilevelItem>
+          <SidebarItem exact={true} to="/farms">
             <span>
-              <HomeIcon />
+              <FontAwesomeIcon icon={FontIcons.faTruck} />
             </span>
 
-            <h3>Lorem</h3>
+            <h3>Farms</h3>
           </SidebarItem>
-          <SidebarItem to="/Ipsum">
+          <SidebarItem exact={true} to="/pools">
             <span>
-              <MonetizationOnIcon />
+              <FontAwesomeIcon icon={FontIcons.faGlassWhiskey} />
             </span>
 
-            <h3>ipsum</h3>
+            <h3>Pools</h3>
           </SidebarItem>
-        </MultilevelItem>
+          <SidebarItem exact={true} to="/prediction">
+            <span>
+              <FontAwesomeIcon icon={FontIcons.faMagic} />
+            </span>
 
-        <SidebarItem to="/Dolret">
-          <span>
-            <MonetizationOnIcon />
-          </span>
+            <h3>Prediction</h3>
+          </SidebarItem>
+          <SidebarItem exact={true} to="/lottery">
+            <span>
+              <FontAwesomeIcon icon={FontIcons.faTicketAlt} />
+            </span>
 
-          <h3>Dolret</h3>
-        </SidebarItem>
+            <h3>Lottery</h3>
+          </SidebarItem>
+          <SidebarItem exact={true} to="/collectibles">
+            <span>
+              <FontAwesomeIcon icon={FontIcons.faShapes} />
+            </span>
+
+            <h3>Collectibles</h3>
+          </SidebarItem>
+          <SidebarItem exact={true} to="/team_battle">
+            <span>
+              <FontAwesomeIcon icon={FontIcons.faTrophy} />
+            </span>
+
+            <h3>Team Battle</h3>
+          </SidebarItem>
+          <MultilevelItem icon={FontIcons.faUsers} title="Teams & Profile">
+            <SidebarItem exact={true} to="/leaderboard">
+              <h3>Leaderboard</h3>
+            </SidebarItem>
+            <SidebarItem exact={true} to="/task_center">
+              <h3>Task Center</h3>
+            </SidebarItem>
+            <SidebarItem exact={true} to="/your_profile">
+              <h3>Your Profile</h3>
+            </SidebarItem>
+          </MultilevelItem>
+
+          <SidebarItem exact={true} to="/info">
+            <span>
+              <FontAwesomeIcon icon={FontIcons.faInfo} />
+            </span>
+
+            <h3>Info</h3>
+          </SidebarItem>
+          <SidebarItem exact={true} to="/ifo">
+            <span>
+              <FontAwesomeIcon icon={FontIcons.faShoppingBag} />
+            </span>
+
+            <h3>IFO</h3>
+          </SidebarItem>
+          <MultilevelItem icon={FontIcons.faEllipsisH} title="More">
+            <SidebarItem exact={true} to="/contact">
+              <h3>Contact</h3>
+            </SidebarItem>
+            <SidebarItem exact={true} to="/voting">
+              <h3>Voting</h3>
+            </SidebarItem>
+            <SidebarItem exact={true} to="/github">
+              <h3>Github</h3>
+            </SidebarItem>
+            <SidebarItem exact={true} to="/docs">
+              <h3>Docs</h3>
+            </SidebarItem>
+            <SidebarItem exact={true} to="/blog">
+              <h3>Blog</h3>
+            </SidebarItem>
+            <SidebarItem exact={true} to="/merch">
+              <h3>Merch</h3>
+            </SidebarItem>
+          </MultilevelItem>
+        </div>
 
         <SidebarFooter />
       </nav>
